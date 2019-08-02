006ce687c7cc5479c83128ef2d91f21634af4234
Musa Barighzaai <mbarighzaai@icloud.com>
2019-08-02 10:43:27 -0400

    add: domainify to parse input to make domain compatible

---
+++ domainify/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+	"unicode"
+)
+
+var tlds = []string{"com", "net"}
+
+const allowedChars = "abcdefghijklmnopqrstuvwxyz0123456789_-"
+
+func main() {
+
+	var tldList = flag.String("tlds", "", "list of tlds, seperated by comma (ex: com,net,org)")
+	flag.Parse()
+
+	if string(*tldList) != "" {
+		t := strings.Split(*tldList, ",")
+		tlds = t
+	}
+
+	rand.Seed(time.Now().UTC().UnixNano())
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		text := strings.ToLower(s.Text())
+		var newText []rune
+		for _, r := range text {
+			if unicode.IsSpace(r) {
+				r = '-'
+			}
+			if !strings.ContainsRune(allowedChars, r) {
+				continue
+			}
+			newText = append(newText, r)
+		}
+		fmt.Println(string(newText) + "." +
+			tlds[rand.Intn(len(tlds))])
+	}
+}
